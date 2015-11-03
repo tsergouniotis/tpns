c6f94b0532f549db56ececba241d043dd68afc12
PanosZografos <User@SuperMicro>
2015-11-03 18:11:09 +0200

    added commands and files for wildfly container

--- tpns-docker-images/util/dockerrun.sh
+++ tpns-docker-images/util/dockerrun.sh
@@ -1,2 +1,5 @@
 # DATABASE
-docker run -it --name=panosdb  -e POSTGRES_PASSWORD=123456 -e TPNS_PASS=123456 pzografos/tpns-database
+docker run -it --name=tpns_db  -e POSTGRES_PASSWORD=123456 -e TPNS_PASS=123456 pzografos/tpns-database
+
+# APPLICATION SERVER
+docker run --name=tpns_be_server -d pzografos/tpns-applicationserver
