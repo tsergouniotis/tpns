23f01929cb409b0725d026771e4739677aa2cd4d
pzografos <pzograf@hotmail.com>
2015-11-04 21:59:30 +0200

    finalized wildfly image

--- tpns-docker-images/applicationserver/wildfly/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Trying to connect postgres"
-while ! curl http://$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT/
-do
-  echo "$(date) - still trying"
-  sleep 1
-done
-echo "$(date) - connected successfully"
-exec "/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0"
