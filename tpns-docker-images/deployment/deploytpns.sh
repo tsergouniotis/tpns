25076ccef1d1689bb7057d97094134224ac759af
PanosZografos <User@SuperMicro>
2015-11-05 13:34:48 +0200

    improved name and removed start up script

--- tpns-docker-images/deployment/deploytpns.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Pulling tpns images ... "
-docker pull pzografos\/tpns-database
-docker pull pzografos\/tpns-appserver
-
-echo "Done!"
-
-exec "$0";
--- tpns-docker-images/util/dockerrun.sh
+++ tpns-docker-images/util/dockerrun.sh
@@ -1,5 +1,5 @@
 # DATABASE
-docker run -it --name=tpns_db  -e POSTGRES_PASSWORD=123456 -e TPNS_PASS=123456 -p 5432:5432 pzografos/tpns-database
+docker run -d --name=tpns_db  -e POSTGRES_PASSWORD=123456 -e TPNS_PASS=123456 -p 5432:5432 pzografos/tpns-database
 
 # APPLICATION SERVER
-docker run --name=tpns_be_server -it --link tpns_db:postgres pzografos/tpns-applicationserver
+docker run -it --name=tpns_appserver --link tpns_db:postgres pzografos/tpns-applicationserver
