43de52ae79fc497f849fa33d7c3e5f8d6dc13116
PanosZografos <User@SuperMicro>
2015-11-05 16:32:10 +0200

    fixed image name

--- tpns-docker-images/util/dockerrun.sh
+++ tpns-docker-images/util/dockerrun.sh
@@ -2,4 +2,4 @@
 docker run -d --name=tpns_db  -e POSTGRES_PASSWORD=123456 -e TPNS_PASS=123456 -p 5432:5432 pzografos/tpns-database
 
 # APPLICATION SERVER
-docker run -it --name=tpns_appserver --link tpns_db:postgres pzografos/tpns-applicationserver
+docker run -it --name=tpns_appserver --link tpns_db:postgres pzografos/tpns-appserver
