1f33288d49c9e8ba03010a48711a5ebbdfc567a3
Thanasis Sergouniotis <tsergouniotis@gmail.com>
2015-11-15 23:40:23 +0200

    Create news app

---
+++ tpns-article-service/src/main/java/com/tpns/article/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.tpns.article.services;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import com.tpns.article.domain.Category;
+import com.tpns.article.repository.CategoryDAO;
+
+@Stateless
+public class CategoryService {
+
+	@EJB
+	private CategoryDAO categoryDAO;
+
+	public List<Category> getCategories() {
+		return categoryDAO.findAll();
+	}
+
+}
--- tpns-main-war/src/main/java/com/tpns/article/managed/beans/ArticleBean.java
+++ tpns-main-war/src/main/java/com/tpns/article/managed/beans/ArticleBean.java
@@ -1,25 +1,35 @@
 package com.tpns.article.managed.beans;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 
 import com.tpns.article.domain.Article;
+import com.tpns.article.domain.Category;
 import com.tpns.article.services.ArticleService;
+import com.tpns.article.services.CategoryService;
 
 @ManagedBean
-@SessionScoped
-public class ArticleBean {
+@RequestScoped
+public class ArticleBean implements Serializable {
 
 	@EJB
 	private ArticleService articleService;
+
+	@EJB
+	private CategoryService categoryService;
 
 	private String name;
 
 	public List<Article> getArticles() {
 		return articleService.findAll();
+	}
+
+	public List<Category> getCategories() {
+		return categoryService.getCategories();
 	}
 
 	public String getName() {
