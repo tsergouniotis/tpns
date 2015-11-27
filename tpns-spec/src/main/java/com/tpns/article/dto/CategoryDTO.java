a0fd57d98b4a61bc6f9c65de871b5e263ecc2a17
Thanasis Sergouniotis <tsergouniotis@gmail.com>
2015-11-27 17:23:00 +0200

    message-bundle is case sensitive

--- tpns-spec/src/main/java/com/tpns/article/dto/CategoryDTO.java
+++ tpns-spec/src/main/java/com/tpns/article/dto/CategoryDTO.java
@@ -39,7 +39,6 @@ public class CategoryDTO implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
@@ -53,11 +52,6 @@ public class CategoryDTO implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		CategoryDTO other = (CategoryDTO) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
