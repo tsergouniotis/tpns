a4162a46bb3f03239c8752098bb2cae4e75a49ce
PanosZografos <User@SuperMicro>
2015-12-01 18:31:38 +0200

    article rendering in main page

--- tpns-main-war/src/main/webapp/resources/js/newsclient/main.js
+++ tpns-main-war/src/main/webapp/resources/js/newsclient/main.js
@@ -16,8 +16,7 @@ $(document).ready(function(){
         accept: "application/json",
         dataType: "json",
         success: function(data) {
-        	printHeadArticle(data);
-        	printAllArticles(data);
+        	printArticles(data);
         }
     });		
 }) 
@@ -31,8 +30,53 @@ function printCategories(data) {
     document.getElementById("catMenu").innerHTML = output;
 }
 
-function printHeadArticle(data) { 	
+function printArticles(data) { 	
+	var count=0;
+	var articleOutput;
+	var topComponentContext ="<p>No articles found!</p>";
+	var bottomComponentContext ="<p>Visit the admin page to load more articles</p>";
+    for (var i in data) {
+    	if (count == 0) {
+    		topComponentContext ="";
+    	}
+    	if (count == 3 ) {
+    		bottomComponentContext ="";
+    	}
+    	if ((count%3) ==0){
+    		articleOutput="<article class=\"tripleblocks tripleleftblock\">";
+    	}
+    	if ((count%3) ==1){
+    		articleOutput="<article class=\"tripleblocks triplemiddleblock\">";
+    	}
+    	if ((count%3) ==2){
+    		articleOutput="<article class=\"tripleblocks triplerightblock\">";
+    	}
+    	articleOutput+="<a href=\"#\"><img class=\"thumbnail\"  height=\"150\" width=\"250\" src=\"";
+    	articleOutput+= getImageFromArticle(data[i]);
+    	articleOutput+= "\" \"/>";
+    	articleOutput+="<span class=\"caption\"><b>"
+    	articleOutput+= data[i].subject;
+    	articleOutput+= "</b></span></a>";
+    	articleOutput+="<p>"
+    	articleOutput+=	data[i].shortDescription
+    	articleOutput+= "</p>";
+    	articleOutput+="</article>";
+    	if ((count%6)<3){
+    		topComponentContext+=articleOutput;
+    	} else {
+    		bottomComponentContext+=articleOutput;
+    	}
+    	count = count + 1;
+	}
+    document.getElementById("topArticleContainer").innerHTML = topComponentContext;
+    document.getElementById("bottomArticleContainer").innerHTML = bottomComponentContext;
 }
 
-function printAllArticles(data) { 	
+function getImageFromArticle(article){
+	if (article.imageUrls.length>0){
+		return article.imageUrls[0];
+	}
+	return "http://www.optv.org/s/photogallery/img/no-image-available.jpg";
 }
+
+
