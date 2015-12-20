a878991099053c6a61020dcc958e3324468b470e
PanosZografos <User@SuperMicro>
2015-12-20 18:01:48 +0200

    news client fron end

--- tpns-main-war/src/main/webapp/resources/js/newsclient/article.js
+++ tpns-main-war/src/main/webapp/resources/js/newsclient/article.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 	setLinkToMain();
+	var articleId = getParameterByName('articleId');
+	loadArticle(articleId);
 }) 
 
 
-function loadAllArticles(aritcleId){
+function loadArticle(aritcleId){
 	$.ajax({ 
         type: "GET",
         url: window.location.origin+"/article-service/v1/article/"+aritcleId,
@@ -11,18 +13,38 @@ function loadAllArticles(aritcleId){
         accept: "application/json",
         dataType: "json",
         success: function(data) {
-        	printArticles(data);
+        	printArticle(data);
         }
     });	
 }
 
-function printArticles(data) { 	
-    document.getElementById("articleTitle").innerHTML = "Article by replace ment";
-    document.getElementById("articleImage").innerHTML = "http://www.optv.org/s/photogallery/img/no-image-available.jpg";
-    document.getElementById("articleContent").innerHTML = "Article Content by replacement";
+function printArticle(data) { 	
+    document.getElementById("articleTitle").innerHTML = "<span  class=\"articleHeader\"><h2>"+data.subject+"</h2></span>";
+    var articleContent ="";
+    articleContent+="<img class=\"articleImg\" src=\"";
+    articleContent+=getImageFromArticle(data);
+    articleContent+="\" />";
+    articleContent+="<p class=\"articleText\" >"+data.content+"</p>";		
+
+    document.getElementById("articleContentDiv").innerHTML =  articleContent;
 }
 
 function setLinkToMain(){
 	var adminLocation = window.location.origin+"/news";	
 	document.getElementById("backContainer").innerHTML = "<a class=\"backToMainLink\" href=\""+adminLocation+"\">Back</a>";
 }
+
+function getImageFromArticle(article){
+	if (article.imageUrls.length>0){
+		return article.imageUrls[0];
+	}
+	return "http://www.optv.org/s/photogallery/img/no-image-available.jpg";
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
