98617b4db590d6136e38a66e9f024e6e8003e58c
Thanasis Sergouniotis <tsergouniotis@gmail.com>
2015-12-23 12:31:40 +0200

    Create configuration using cli scripts

--- tpns-database/dbscripts/tpns-data.sql
+++ tpns-database/dbscripts/tpns-data.sql
@@ -2,8 +2,8 @@
 -- Update Database Script
 -- *********************************************************************
 -- Change Log: src/main/db/db.changelog-master.xml
--- Ran at: 11/7/15 8:57 PM
--- Against: postgres@jdbc:postgresql://localhost:5432/tpns
+-- Ran at: 12/23/15 11:48 AM
+-- Against: tpns@jdbc:postgresql://localhost:5432/tpns
 -- Liquibase version: 3.3.5
 -- *********************************************************************
 
@@ -16,7 +16,7 @@ DELETE FROM public.databasechangeloglock;
 INSERT INTO public.databasechangeloglock (ID, LOCKED) VALUES (1, FALSE);
 
 -- Lock Database
-UPDATE public.databasechangeloglock SET LOCKED = TRUE, LOCKEDBY = 'fe80:0:0:0:fc05:39ff:fe4a:d574%vethcde8b73 (fe80:0:0:0:fc05:39ff:fe4a:d574%vethcde8b73)', LOCKGRANTED = '2015-11-07 20:57:59.370' WHERE ID = 1 AND LOCKED = FALSE;
+UPDATE public.databasechangeloglock SET LOCKED = TRUE, LOCKEDBY = 'fe80:0:0:0:c82f:53ff:fefc:af62%veth8e32311 (fe80:0:0:0:c82f:53ff:fefc:af62%veth8e32311)', LOCKGRANTED = '2015-12-23 11:48:21.989' WHERE ID = 1 AND LOCKED = FALSE;
 
 -- Create Database Change Log Table
 CREATE TABLE public.databasechangelog (ID VARCHAR(255) NOT NULL, AUTHOR VARCHAR(255) NOT NULL, FILENAME VARCHAR(255) NOT NULL, DATEEXECUTED TIMESTAMP WITHOUT TIME ZONE NOT NULL, ORDEREXECUTED INT NOT NULL, EXECTYPE VARCHAR(10) NOT NULL, MD5SUM VARCHAR(35), DESCRIPTION VARCHAR(255), COMMENTS VARCHAR(255), TAG VARCHAR(255), LIQUIBASE VARCHAR(20));
@@ -58,7 +58,7 @@ CREATE SEQUENCE public.categoryseq START WITH 1 INCREMENT BY 1;
 INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('create-categories-table', 'thanasis.sergouniotis', 'src/main/db/1.0.0/ddl/ddl.xml', NOW(), 4, '7:ffabe6dfc0a04d220624df32f9020f95', 'createTable, addPrimaryKey, createSequence', '', 'EXECUTED', '3.3.5');
 
 -- Changeset src/main/db/1.0.0/ddl/ddl.xml::create-articles-table::thanasis.sergouniotis
-CREATE TABLE public.articles (article_id BIGINT NOT NULL, subject VARCHAR(255), content VARCHAR(2096), category_id SMALLINT, image VARCHAR(2096), video VARCHAR(2096), created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(), updated_at TIMESTAMP WITHOUT TIME ZONE);
+CREATE TABLE public.articles (article_id BIGINT NOT NULL, subject VARCHAR(255), content VARCHAR(20480), short_description VARCHAR(1024), category_id SMALLINT, author_id BIGINT, status VARCHAR(50), created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(), updated_at TIMESTAMP WITHOUT TIME ZONE, posted_at TIMESTAMP WITHOUT TIME ZONE, version INT);
 
 ALTER TABLE public.articles ADD CONSTRAINT articles_pk PRIMARY KEY (article_id);
 
@@ -66,7 +66,41 @@ CREATE SEQUENCE public.articleseq START WITH 1 INCREMENT BY 1;
 
 ALTER TABLE public.articles ADD CONSTRAINT article_category_fk FOREIGN KEY (category_id) REFERENCES public.categories (category_id);
 
-INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('create-articles-table', 'thanasis.sergouniotis', 'src/main/db/1.0.0/ddl/ddl.xml', NOW(), 5, '7:d2dfb90150fb3a4094e51b74ca2900a2', 'createTable, addPrimaryKey, createSequence, addForeignKeyConstraint', '', 'EXECUTED', '3.3.5');
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('create-articles-table', 'thanasis.sergouniotis', 'src/main/db/1.0.0/ddl/ddl.xml', NOW(), 5, '7:8f85830a60f5b56e10f33e4e1174c57b', 'createTable, addPrimaryKey, createSequence, addForeignKeyConstraint', '', 'EXECUTED', '3.3.5');
+
+-- Changeset src/main/db/1.0.0/ddl/ddl.xml::create-media-resources-table::panos.zografos
+CREATE TABLE public.articlemediaresources (article_id BIGINT NOT NULL, resource_type VARCHAR(50), url VARCHAR(2048));
+
+ALTER TABLE public.articlemediaresources ADD CONSTRAINT article_resource_fk FOREIGN KEY (article_id) REFERENCES public.articles (article_id);
+
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('create-media-resources-table', 'panos.zografos', 'src/main/db/1.0.0/ddl/ddl.xml', NOW(), 6, '7:96ce43a1aafcf705d84b5fdd7ded02d7', 'createTable, addForeignKeyConstraint', '', 'EXECUTED', '3.3.5');
+
+-- Changeset src/main/db/1.0.0/ddl/ddl.xml::create-keyword-tables::thanasis.sergouniotis
+CREATE TABLE public.keywords (keyword VARCHAR(1024) NOT NULL, version INT);
+
+ALTER TABLE public.keywords ADD CONSTRAINT keyword_pk PRIMARY KEY (keyword);
+
+CREATE TABLE public.article_keywords (keyword VARCHAR(1024) NOT NULL, article_id BIGINT NOT NULL);
+
+ALTER TABLE public.article_keywords ADD CONSTRAINT article_keyword_keyword_fk FOREIGN KEY (keyword) REFERENCES public.keywords (keyword);
+
+ALTER TABLE public.article_keywords ADD CONSTRAINT article_keyword_article_fk FOREIGN KEY (article_id) REFERENCES public.articles (article_id);
+
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('create-keyword-tables', 'thanasis.sergouniotis', 'src/main/db/1.0.0/ddl/ddl.xml', NOW(), 7, '7:d18b02d292144410eec720b263c97e92', 'createTable, addPrimaryKey, createTable, addForeignKeyConstraint (x2)', '', 'EXECUTED', '3.3.5');
+
+-- Changeset src/main/db/1.0.0/ddl/ddl.xml::create-applications-table::thanasis.sergouniotis
+CREATE TABLE public.applications (application_id VARCHAR(255) NOT NULL, endpoint VARCHAR(255) NOT NULL, client_id VARCHAR(255) NOT NULL, version INT);
+
+ALTER TABLE public.applications ADD CONSTRAINT application_pk PRIMARY KEY (application_id);
+
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('create-applications-table', 'thanasis.sergouniotis', 'src/main/db/1.0.0/ddl/ddl.xml', NOW(), 8, '7:2f130c968a095893940edd6c306d187c', 'createTable, addPrimaryKey', '', 'EXECUTED', '3.3.5');
+
+-- Changeset src/main/db/1.0.0/ddl/ddl.xml::create-application-params-table::thanasis.sergouniotis
+CREATE TABLE public.application_parameters (key VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL);
+
+ALTER TABLE public.application_parameters ADD CONSTRAINT application_parameters_pk PRIMARY KEY (key);
+
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('create-application-params-table', 'thanasis.sergouniotis', 'src/main/db/1.0.0/ddl/ddl.xml', NOW(), 9, '7:3295fb0d14d991fd993adb8a72c8c95c', 'createTable, addPrimaryKey', '', 'EXECUTED', '3.3.5');
 
 -- Changeset src/main/db/1.0.0/dml/dml.xml::insert-roles::thanasis.sergouniotis
 INSERT INTO public.roles (role_id, role_name) VALUES ('1', 'ADMIN');
@@ -75,16 +109,20 @@ INSERT INTO public.roles (role_id, role_name) VALUES ('2', 'AUTHOR');
 
 INSERT INTO public.roles (role_id, role_name) VALUES ('3', 'CHIEF_EDITOR');
 
-INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-roles', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 6, '7:53b9929afc81e836e68f6a21c1563d90', 'insert (x3)', '', 'EXECUTED', '3.3.5');
+INSERT INTO public.roles (role_id, role_name) VALUES ('4', 'APPLICATION');
+
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-roles', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 10, '7:83ec779d07abacf8da112065f0b356cf', 'insert (x4)', '', 'EXECUTED', '3.3.5');
 
 -- Changeset src/main/db/1.0.0/dml/dml.xml::insert-users::thanasis.sergouniotis
 INSERT INTO public.users (user_id, username, password) VALUES (nextval('userseq'), 'admin', 'admin');
 
-INSERT INTO public.users (user_id, username, password) VALUES (nextval('userseq'), 'author', 'author');
+INSERT INTO public.users (user_id, username, password, firstname, surname) VALUES (nextval('userseq'), 'author', 'author', 'Carl', 'Bernstein');
 
 INSERT INTO public.users (user_id, username, password) VALUES (nextval('userseq'), 'chief', 'chief');
 
-INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-users', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 7, '7:81006e7d715a32eb2161aa1f08b6de15', 'insert (x3)', '', 'EXECUTED', '3.3.5');
+INSERT INTO public.users (user_id, username, password) VALUES (nextval('userseq'), 'onsports', 'onsports');
+
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-users', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 11, '7:ec3f6f118d59ca77b6b80ac02913d1db', 'insert (x4)', '', 'EXECUTED', '3.3.5');
 
 -- Changeset src/main/db/1.0.0/dml/dml.xml::insert-users-roles::thanasis.sergouniotis
 INSERT INTO public.users_roles (role_id, user_id) VALUES ((select role_id from roles where role_name='ADMIN'), (select user_id from users where username='admin'));
@@ -93,7 +131,9 @@ INSERT INTO public.users_roles (role_id, user_id) VALUES ((select role_id from r
 
 INSERT INTO public.users_roles (role_id, user_id) VALUES ((select role_id from roles where role_name='CHIEF_EDITOR'), (select user_id from users where username='chief'));
 
-INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-users-roles', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 8, '7:4553bbc59dece2684b63571db8b73a39', 'insert (x3)', '', 'EXECUTED', '3.3.5');
+INSERT INTO public.users_roles (role_id, user_id) VALUES ((select role_id from roles where role_name='APPLICATION'), (select user_id from users where username='onsports'));
+
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-users-roles', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 12, '7:9add2967456fcf627ea9ddaddad2ebf5', 'insert (x4)', '', 'EXECUTED', '3.3.5');
 
 -- Changeset src/main/db/1.0.0/dml/dml.xml::insert-categories::thanasis.sergouniotis
 INSERT INTO public.categories (category_id, category_name) VALUES (nextval('CATEGORYSEQ'), 'politics');
@@ -106,7 +146,12 @@ INSERT INTO public.categories (category_id, category_name) VALUES (nextval('CATE
 
 INSERT INTO public.categories (category_id, category_name) VALUES (nextval('CATEGORYSEQ'), 'social');
 
-INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-categories', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 9, '7:04faa6e77909a5aaf773983a306a9c3c', 'insert (x5)', '', 'EXECUTED', '3.3.5');
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-categories', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 13, '7:04faa6e77909a5aaf773983a306a9c3c', 'insert (x5)', '', 'EXECUTED', '3.3.5');
+
+-- Changeset src/main/db/1.0.0/dml/dml.xml::insert-application-parameters::thanasis.sergouniotis
+INSERT INTO public.application_parameters (key, value) VALUES ('article.lucene.directory', '/home/sergouniotis/Downloads/tpns/index');
+
+INSERT INTO public.databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, LIQUIBASE) VALUES ('insert-application-parameters', 'thanasis.sergouniotis', 'src/main/db/1.0.0/dml/dml.xml', NOW(), 14, '7:54ce07f0b0077ae24550d91976b2e5b5', 'insert', '', 'EXECUTED', '3.3.5');
 
 -- Release Database Lock
 UPDATE public.databasechangeloglock SET LOCKED = FALSE, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
--- tpns-docker-images/applicationserver/wildfly/run.sh
+++ tpns-docker-images/applicationserver/wildfly/run.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
+
 set -e
 
-sleep 20
+JBOSS_HOME=/opt/jboss/wildfly
+JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
+JBOSS_MODE=${1:-"standalone"}
+JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
 
-/opt/jboss/wildfly/bin/standalone.sh -b '0.0.0.0'
-exec "$@"
+function wait_for_server() {
+  #until `$JBOSS_CLI -c "ls /deployment" &> /dev/null`; do
+  #  sleep 1
+  #done
+  sleep 20
+}
+
+echo "=> Starting WildFly server"
+$JBOSS_HOME/bin/standalone.sh -c standalone-full.xml &
+
+echo "=> Waiting for the server to boot"
+wait_for_server
+
+echo "=> Executing the commands"
+$JBOSS_HOME/bin/jboss-cli.sh --file=/tpns.cli
+
+/bin/bash
+
+#echo "=> Shutting down WildFly"
+#if [ "$JBOSS_MODE" = "standalone" ]; then
+#  $JBOSS_CLI -c ":shutdown"
+#else
+#  $JBOSS_CLI -c "/host=*:shutdown"
+#fi
