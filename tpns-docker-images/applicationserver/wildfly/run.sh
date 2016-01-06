786fcbc06587fb928ef3ea891b20770d6e50885a
PanosZografos <User@SuperMicro>
2016-01-06 17:01:05 +0200

    fixed application server deployment

--- tpns-docker-images/applicationserver/wildfly/run.sh
+++ tpns-docker-images/applicationserver/wildfly/run.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
       
-JBOSS_HOME=/opt/wildfly
+JBOSS_HOME=/opt/jboss/wildfly
 JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
-JBOSS_MODE=${1:-"standalone"}
-JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
   
 function wait_for_server() {
-  until `$JBOSS_CLI -c "ls /deployment" &> /dev/null`; do
-    sleep 1
-  done
+ # until `$JBOSS_CLI -c "ls /deployment" &> /dev/null`; do
+ #   sleep 1
+ # done
+ sleep 20
 }
   
 echo "=> Starting WildFly server"
-$JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG >dev/null &
+$JBOSS_HOME/bin/standalone.sh -c standalone.xml &> /dev/null &
   
-echo "=> Waiting for the server to boot"
+echo "=> Waiting for the server to boot and database to be setup"
 wait_for_server
   
 echo "=> Executing the commands"
-$JBOSS_CLI -c --file=/tpns.cli
-  
-echo "=> Shutting down WildFly"
-if [ "$JBOSS_MODE" = "standalone" ]; then
-  $JBOSS_CLI -c ":shutdown"
-else
-  $JBOSS_CLI -c "/host=*:shutdown"
-fi
+$JBOSS_CLI -c --file=/tpns/tpns.cli
+
+echo "=> Restarting wildfly"
+$JBOSS_HOME/bin/standalone.sh -c standalone.xml -b 0.0.0.0
+
