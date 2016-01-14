53ba2883bfc305213050fa224659658225e437af
PanosZografos <User@SuperMicro>
2016-01-14 09:39:05 +0200

    added missing plugins fix scripts

--- tpns-docker-images/util/dockercommands.sh
+++ tpns-docker-images/util/dockercommands.sh
@@ -3,10 +3,13 @@ docker rm $(docker ps -a -q)
 docker rmi `docker images --filter 'dangling=true' -q --no-trunc`
 
 # DATABASE
-docker run -d --name=tpnsdatabase  -e POSTGRES_PASSWORD=123456 -e TPNS_PASS=tpns -p 5432:5432 pzografos/tpns-database
+docker run -it --name=tpnsdatabase  -e POSTGRES_PASSWORD=123456 -e TPNS_PASS=tpns -p 5432:5432 pzografos/tpns-database
 docker build -t pzografos/tpns-database .
 
 # APPLICATION SERVER
 docker run -it --name=tpnsappserver --link tpnsdatabase:postgres -p 8080:8080 pzografos/tpns-appserver
 docker build -t pzografos/tpns-appserver .
 
+# CI Server
+docker run -it -p 8081:8080 -p 50000:50000 -v /home/panos/dev/jenkins:/var/jenkins_home pzografos/tpns-ciserver
+docker build -t pzografos/tpns-ciserver .
